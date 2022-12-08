8ef3cc33a788f9890f67cdee39102ce5aaedff2d
rofiadhimPS <muhammadrofiadhim@gmail.com>
2022-12-08 10:42:31 +0800

    vendor

--- laracamp/vendor/composer/autoload_real.php
+++ laracamp/vendor/composer/autoload_real.php
@@ -22,8 +22,6 @@ class ComposerAutoloaderInit1055ebb623466054fe8731534277ee7c
             return self::$loader;
         }
 
-        require __DIR__ . '/platform_check.php';
-
         spl_autoload_register(array('ComposerAutoloaderInit1055ebb623466054fe8731534277ee7c', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInit1055ebb623466054fe8731534277ee7c', 'loadClassLoader'));
