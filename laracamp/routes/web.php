a0f4630f110838300be8dec8f341a7d753faf48d
rofiadhimPS <muhammadrofiadhim@gmail.com>
2022-12-10 17:22:52 +0800

    integrasi checkout

--- laracamp/routes/web.php
+++ laracamp/routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\User\CheckoutController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,13 +20,8 @@ Route::get('/', function () {
     return view('welcome');
 })->name('welcome');
 
-Route::get('checkout/{camp:slug}', function () {
-    return view('checkout');
-})->name('checkout');
-
-Route::get('success-checkout', function () {
-    return view('success_checkout');
-})->name('success-checkout');
+Route::get('checkout/success', [CheckoutController::class, 'success'])->name('checkout.success');
+Route::get('checkout/{camp:slug}', [CheckoutController::class, 'create'])->name('checkout.create');
 
 //socialize routes
 Route::get('sign-in-google', [UserController::class, 'google'])->name('user.login.google');
