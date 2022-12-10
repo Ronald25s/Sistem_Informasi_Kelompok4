4c2ff7b7efd22a2f63913d4c5a8365cd0e130bc8
rofiadhimPS <muhammadrofiadhim@gmail.com>
2022-12-11 01:39:31 +0800

    validasi checkout

--- laracamp/app/Http/Controllers/User/CheckoutController.php
+++ laracamp/app/Http/Controllers/User/CheckoutController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\User;
 use App\Models\Checkout;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Requests\User\Checkout\Store;
 use App\Models\Camp;
 use Auth;
 
@@ -38,7 +39,7 @@ class CheckoutController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request, Camp $camp)
+    public function store(Store $request, Camp $camp)
     {
         // mapping request data
         $data = $request->all();
