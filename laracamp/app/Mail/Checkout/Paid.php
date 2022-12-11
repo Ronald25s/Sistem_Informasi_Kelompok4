6fcd1ffce24bf03e4e265cb715a60e17bbd9e2e5
rofiadhimPS <muhammadrofiadhim@gmail.com>
2022-12-11 16:48:16 +0800

    integrated paid email from admin

---
+++ laracamp/app/Mail/Checkout/Paid.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Mail\Checkout;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class Paid extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    private $checkout;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($checkout)
+    {
+        $this->checkout = $checkout;
+    }
+
+    public function build()
+    {
+        return $this->subject('Your Transaction Has Been Confirmed')->markdown('emails.checkout.paid', [
+            'checkout' => $this->checkout
+        ]);
+    }
+    
+    /**
+     * Get the message envelope.
+     *
+     * @return \Illuminate\Mail\Mailables\Envelope
+     */
+    public function envelope()
+    {
+        return new Envelope(
+            subject: 'Paid',
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     *
+     * @return \Illuminate\Mail\Mailables\Content
+     */
+    public function content()
+    {
+        return new Content(
+            markdown: 'emails.checkout.paid',
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array
+     */
+    public function attachments()
+    {
+        return [];
+    }
+}
