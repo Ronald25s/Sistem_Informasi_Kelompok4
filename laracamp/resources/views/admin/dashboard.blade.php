bd9a86982dd9f1d362b9e9621c0b10f01dfa198d
rofiadhimPS <muhammadrofiadhim@gmail.com>
2022-12-11 16:19:06 +0800

    Admin Paid

--- laracamp/resources/views/admin/dashboard.blade.php
+++ laracamp/resources/views/admin/dashboard.blade.php
@@ -34,10 +34,12 @@
                                             @endif
                                         </td>
                                         <td>
-                                            <form action="" method="POST">
+                                            @if (!$checkout->is_paid)
+                                            <form action="{{route('admin.checkout.update', $checkout->id)}}" method="POST">
                                                 @csrf
                                                 <button class="btn btn-primary btn-sm">Set to Paid</button>
                                             </form>
+                                            @endif
                                         </td>
                                     </tr>
                                 @empty
